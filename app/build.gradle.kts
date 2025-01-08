ccc3074650046bf8b35ba476267d792e9c2c1691
mohamedkeshawy <mokeshawy@gmail.com>
2025-01-08 19:00:35 +0200

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,146 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.mapsplatform.secrets.gradle.plugin)
+    alias(libs.plugins.google.services)
+    alias(libs.plugins.firebase.crashlytics)
+}
+
+android {
+    namespace = "com.tawuniya.userinfo"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.tawuniya.userinfo"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        debug {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    flavorDimensions += listOf("version")
+    productFlavors {
+        create("staging") {
+            dimension = "version"
+            versionNameSuffix = ".stage"
+        }
+        create("live") {
+            dimension = "version"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // compose viewModel
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    // navigation-compose
+    implementation(libs.androidx.navigation.compose)
+
+
+    // maps
+    implementation(libs.maps.compose)
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
+    implementation(libs.play.services.places)
+    implementation(libs.places)
+
+
+    //FireBase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.bundles.firebase.bundle)
+
+
+    //process-phoenix
+    implementation(libs.process.phoenix)
+
+    // timber
+    implementation(libs.timber)
+
+    //Android Remote Debugger
+    debugImplementation(libs.debugger)
+    releaseImplementation(libs.noop)
+
+    //Play Store App Update Libraries
+    implementation(libs.app.update)
+    implementation(libs.app.update.ktx)
+
+    //Paging compose library
+    implementation(libs.androidx.paging.runtime.ktx)
+    implementation(libs.androidx.paging.compose)
+
+    //Coil
+    implementation(libs.coil)
+    implementation(libs.coil.svg)
+
+    // Lottie
+    implementation(libs.lottie.compose)
+
+    // encrypted shared preference
+    implementation(libs.androidx.security.crypto)
+
+    // Ktor
+    implementation(libs.bundles.ktor.bundle)
+
+
+    // logging - interceptor
+    implementation(libs.logging.interceptor)
+
+    // koin
+    implementation(libs.koin.android)
+
+    //Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.mapsplatform.secrets.gradle.plugin) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.hilt.plugin) apply false
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.google.services)
+        classpath(libs.firebase.perf.plugin)
+        classpath(libs.firebase.crashlytics.gradle)
+        classpath(libs.easylauncher)
+    }
+}
